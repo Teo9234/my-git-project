5fcae501dae5606d59a77889b678c5cc60b051e5
Teo <potheodoros@gmail.com>
2025-11-20 20:47:03 +0200

    initial commit

---
+++ src/gr/aueb/sev/mygitpro/HelloWorld.java
@@ -0,0 +1,15 @@
+package gr.aueb.sev.mygitpro;
+
+/**
+ * Hello world program.
+ * For showing how to make a git project.
+ * How to stage and commit changes.
+ * Also, how to push it to GitHub.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+}
